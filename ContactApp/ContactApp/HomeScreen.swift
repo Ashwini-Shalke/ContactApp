2cd69c14212d9c5dbe82121edfea1251e1c2a6f9
Ashwini shalke <ashwinishalke050@gmail.com>
2019-09-17 23:47:00 +0530

    creates NavigationController programaticlly with TableView

---
+++ ContactApp/ContactApp/HomeScreen.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewControler.swift
+//  ContactApp
+//
+//  Created by Ashwini shalke on 17/09/19.
+//  Copyright © 2019 Ashwini Shalke. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeScreen: UITableViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+}
